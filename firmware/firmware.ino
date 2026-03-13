91d50fe3b236bf5ae7575b973c72d723af17e3d8
Jeremy Ellis <keyfreemusic@gmail.com>
2026-03-13 14:12:15 -0700

    Create firmware.ino

---
+++ firmware/firmware.ino
@@ -0,0 +1,21 @@
+/*
+Experiment ID: EXP-EXAMPLE
+
+Hardware:
+Xiao ESP32S3 Sense
+
+Description:
+Template firmware for on-device TinyML experiments.
+*/
+
+void setup() {
+
+  Serial.begin(115200);
+
+  Serial.println("TinyML Experiment Template");
+
+}
+
+void loop() {
+
+}
